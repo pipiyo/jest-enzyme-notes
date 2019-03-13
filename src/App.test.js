80fce11c768aac29635767d80cba09299e074e72
pipiyo <fe.cavieres@gmail.com>
2019-03-13 14:00:32 -0300

    test props

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
 
-import App from './App';
+import App, { Link } from './App';
 
 configure({ adapter: new Adapter() })
 
@@ -31,4 +32,26 @@ describe('<App />', () => {
 	expect(wrapper.find('h1').text()).toBe('Welcome to React')
   })
 
+  it('matches the snapshot', () => {
+    const tree = shallow(<App />)
+    expect(toJson(tree)).toMatchSnapshot()
+  })
+
+})
+
+describe('<Link />', () => {
+  it('link component accepts address prop', () => {
+    const wrapper = shallow(<Link address='www.google.com' />)
+    expect(wrapper.instance().props.address).toBe('www.google.com')
+  })
+  it('a tag node renders href correctly', () => {
+    const wrapper = shallow(<Link address='www.google.com' />)
+    expect(wrapper.props().href).toBe('www.google.com')
+  })
+  it('returns null with true hide prop', () => {
+    const wrapper = shallow(<Link hide={false} />)
+    expect(wrapper.find('a').length).toBe(1)
+    wrapper.setProps({ hide: true })
+    expect(wrapper.get(0)).toBeNull()
+  })
 })
