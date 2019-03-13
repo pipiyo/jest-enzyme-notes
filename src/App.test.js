4d6e7e8c1cf23376f880fb4705e714a9c816250a
pipiyo <fe.cavieres@gmail.com>
2019-03-13 13:26:30 -0300

    Install and shallow second parameter

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() })
+
+describe('<App />', () => {
+  it('should render App', () => {
+    const wrapper = shallow(<App />, {context: {}, disableLifecycleMethods: false})
+    console.log(wrapper.debug())
+  })
+})
